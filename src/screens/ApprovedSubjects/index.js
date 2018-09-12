e5e72b63d8805a6f224e3f5a54c462dacb2168d9
augusto urga <augusto.urga@mercadolibre.com>
2018-09-12 15:57:24 -0300

    frank suggestions

--- src/screens/ApprovedSubjects/index.js
+++ src/screens/ApprovedSubjects/index.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react';
 import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
+import styles from './styles';
+
 
 export default class ApprovedSubjects extends Component {
   render() {
@@ -20,14 +22,4 @@ export default class ApprovedSubjects extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
+
--- src/screens/WorkTime/index.js
+++ src/screens/WorkTime/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {View, Text } from 'react-native';
-import Button from '../../component/Button';
 import styles from './styles';
 
-const Options = [
-  ]
 class WorkTime extends React.Component {
-    
+  state = {
+    dates: [
+      {day: 'Lunes', start: 9, end: 18},
+      {day: 'Martes', start: 9, end: 18},
+      {day: 'Miercoles', start: 9, end: 18},
+      {day: 'Jueves', start: 9, end: 18},
+      {day: 'Viernes', start: 9, end: 18}
+    ]
+  }
   render() {
     return (
-     <View style={styles.container}>
+      <View>
+        {this.state.dates.map(date =>
+        <View key={date.day} style={styles.dayContainer}>
+          <Text style={styles.textField}>{date.day}</Text>
+          <Text style={styles.textField}>{date.start}</Text>
+          <Text style={styles.textField}>{date.end}</Text>
+        </View>)}
       </View>
     )
   }
