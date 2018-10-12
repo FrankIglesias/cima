0af47bed06738ae8ecec7b2d8d3af3cb6b35fac3
augusto urga <augusto.urga@mercadolibre.com>
2018-10-11 23:00:22 -0300

    agrego select en carreer follower

--- src/screens/CareerFollower/index.js
+++ src/screens/CareerFollower/index.js
@@ -1,17 +1,192 @@
 import React from 'react';
-import {View, Text } from 'react-native';
-import Button from '../../component/Button';
 import styles from './styles';
+import { Alert, Text, TextInput, View } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-const Options = [
-    { text: 'Materias Aprobadas', route: 'ApprovedSubjects' },
-    { text: 'Materias Adeudadas', route: 'PersonalSpecifications' },
+const Materias = {
+  /*Faltan electivas, 
+  Esto deberiamos sacarlo de esta clase,
+  ademas estaba pensando que es mejor una lista de checkbox que cuando se clickea, se considera aprobada, que piensan?*/
+  materias: [
+    {
+      label: 'Análisis Matemático I',
+      value: 'AMI ',
+    },
+    {
+      label: 'Álgebra y Geometría Analítica',
+      value: 'ALGEBRA ',
+    },
+    {
+      label: 'Matemática Discreta',
+      value: 'MATDISC',
+    },
+    {
+      label: 'Sistemas y Organizaciones',
+      value: 'SISYORG',
+    },
+    {
+      label: 'Algoritmo y Estructuras de Datos',
+      value: 'ALGO',
+    },
+    {
+      label: 'Arquitectura de Computadoras ',
+      value: 'ARQ',
+    },
+    {
+      label: 'Física I',
+      value: 'FI',
+    },
+    {
+      label: 'Inglés I',
+      value: 'INGLESI',
+    },
+    {
+      label: 'Química',
+      value: 'QUIM',
+    },
+    {
+      label: 'Análisis Matemático II',
+      value: 'AMII',
+    },
+    {
+      label: 'Física II',
+      value: 'FII',
+    },
+    {
+      label: 'Análisis de Sistemas',
+      value: 'ANASIST',
+    },
+    {
+      label: 'Sintaxis y Semántica de los Lenguajes',
+      value: 'SINSEMLEN',
+    },
+    {
+      label: 'Paradigmas de Programación',
+      value: 'PARADIGMAS',
+    },
+    {
+      label: 'Sistemas Operativos',
+      value: 'SISOPS',
+    },
+    {
+      label: 'Sistemas de Representación',
+      value: 'REPRESION',
+    },
+    {
+      label: 'Probabilidades y Estadísticas',
+      value: 'PROBA',
+    },
+    {
+      label: 'Diseño de Sistemas',
+      value: 'DISIS',
+    },
+    {
+      label: 'Comunicaciones ',
+      value: 'COMU',
+    },
+    {
+      label: 'Matemática Superior',
+      value: 'MATESUP',
+    },
+    {
+      label: 'Gestión de Datos',
+      value: 'GDD',
+    },
+    {
+      label: 'Ingeniería y Sociedad',
+      value: 'INGYSOC',
+    },
+    {
+      label: 'Inglés II',
+      value: 'INGII',
+    },
+    {
+      label: 'Redes de Información',
+      value: 'REDES',
+    },
+    {
+      label: 'Administración de Recursos',
+      value: 'ADR',
+    },
+    {
+      label: 'Investigación Operativa',
+      value: 'INVOP',
+    },
+    {
+      label: 'Simulación',
+      value: 'SIMU',
+    },
+    {
+      label: 'Ingeniería de Software',
+      value: 'INGSOFT',
+    },
+    {
+      label: 'Teoría de Control',
+      value: 'TEOCONTROL',
+    },
+    {
+      label: 'Legislación',
+      value: 'LEG',
+    },
+    {
+      label: 'Proyecto Final',
+      value: 'PAJA',
+    },
+    {
+      label: 'Inteligencia Artificial',
+      value: 'IA',
+    },
+    {
+      label: 'Administración Gerencial',
+      value: 'LEG',
+    },
+    {
+      label: 'Sistemas de Gestión ',
+      value: 'SISGES',
+    },
   ]
-function CareerFollower({ navigation }) {
-  return <View style={styles.container}>
-    {Options.map(option =>
-      <Button key={option.route} title={option.text} onPress={() => { navigation.push(option.route) }} />)}
-  </View>
+}
+
+class CareerFollower extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRefs = {};
+    this.state = Materias
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={{ paddingVertical: 5 }} />
+
+        <Text>¿Que materias aprobaste?</Text>
+        <RNPickerSelect
+          placeholder={{
+            label: 'Selecciona una materia',
+            value: null,
+          }}
+          items={this.state.materias}
+          onValueChange={(value) => {
+            this.setState({
+              favSport: value,
+            });
+          }}
+          onUpArrow={() => {
+            this.inputRefs.picker.togglePicker();
+          }}
+          onDownArrow={() => {
+            this.inputRefs.company.focus();
+          }}
+          style={styles.pickerSelectStyles}
+          value={this.state.favSport}
+          ref={(el) => {
+            this.inputRefs.picker2 = el;
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 export default CareerFollower;
