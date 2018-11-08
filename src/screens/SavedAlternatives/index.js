b52e73a64ffabd4d84063d3410d4aab576f14878
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2018-11-08 01:54:52 -0300

    normalize hours

--- src/screens/SavedAlternatives/index.js
+++ src/screens/SavedAlternatives/index.js
@@ -3,8 +3,37 @@ import {View, Text, ScrollView } from 'react-native';
 import styles from './styles';
 import CardView from 'react-native-cardview';
 
-
 import * as PlanificadorService from '../../services/alternativity';
+
+const schedulesToTimes = {
+  m: {
+    0: "07:45",
+    1: "8.30",
+    2: "9.15",
+    3: "10.15",
+    4: "11.00",
+    5: "11.45",
+    6: "12.30"
+  },
+  t: {
+    0: "14.15",
+    1: "15.00",
+    2: "15.45",
+    3: "16.00",
+    4: "16.45",
+    5: "17.30",
+    6: "18.15"
+  },
+  n: {
+    0: "18.15",
+    1: "19.00",
+    2: "19.45",
+    3: "20.30",
+    4: "20.45",
+    5: "21.30",
+    6: "22.15"
+  }
+};
 
 /* El core de nuestra app, esta vista se encarga de mostrar las alternativas que se generan,
 la idea es que tengan una estrellita o un corazoncito que se pueda clickear y asi, se guarde */
@@ -29,7 +58,7 @@ class SavedAlternatives extends Component {
                     {alternativity.schedules.map(subject =>
                      <View>
                       <Text style={styles.cardViewText}>{subject.materia}:</Text>
-                      <Text>{subject.days[0].name} 19:00 a 23:00</Text>
+                      <Text>{subject.days[0].name} {schedulesToTimes[subject.days[0].turn][subject.days[0].startHour]} a {schedulesToTimes[subject.days[0].turn][subject.days[0].endHour]}</Text>
                     </View>
                     )}
                   </View>
