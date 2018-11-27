0bcdf486f26c01a52b3273699222417405500a69
augustourga <augusto.urga@mercadolibre.com>
2018-11-27 16:35:53 -0300

    el vivero de cima

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { View, Linking } from 'react-native';
+import { WebView } from 'react-native';
 
-import Button from '../../component/Button';
-
-import styles from './styles';
-
-const Options = [
-  { text: 'Planificar Cursada', route: 'Planner'},
-  { text: 'Personalizar Alternativas', route: 'WishedSubjects' },
-  { text: 'Perfil', route: 'Profile' },
-  { text: 'Siga', route: 'Siga', onClick: () => { Linking.openURL('http://siga.frba.utn.edu.ar/') } },
-]
-/* Sacarian/agregarian/moverian algo de este menu? Que? */
-function Home({ navigation }) {
-  return <View style={styles.container}>
-    {Options.map(option =>
-      <Button key={option.route} title={option.text} onPress={option.onClick || (() => { navigation.push(option.route) })} />)}
-  </View>
+class Home extends React.Component {
+  render() {
+    return (
+      <WebView
+        source={{ uri: 'https://sear-utn.herokuapp.com/' }}
+        style={{ marginTop: 20 }}
+        javaScriptEnabled
+      />
+    );
+  }
 }
 
 export default Home;
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -7,7 +7,7 @@ export default StyleSheet.create({
   },
   image: {
     width: 250,
-    height: 100,
+    height: 300,
     alignSelf: 'center',
     marginTop: 30
   },
